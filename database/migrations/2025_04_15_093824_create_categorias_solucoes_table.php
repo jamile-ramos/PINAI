0288dbe5c115bb0a7b8ba2155558b83d052d7e78
Jamile Ramos <jamiledasilvaramos@gmail.com>
2025-04-15 10:18:21 -0300

    Criacao das tabelas categorias_solucoes, publicos_alvos e solucoes

---
+++ database/migrations/2025_04_15_093824_create_categorias_solucoes_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('categorias_solucoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomeCategoria', 255);
+            $table->enum('status', ['ativo', 'inativo'])->default('ativo');
+            $table->foreignId('idUsuario')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias_solucoes');
+    }
+};
---
+++ database/migrations/2025_04_15_100636_create_solucoes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('solucoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 255);
+            $table->text('descricao');
+            $table->text('passosImplementacao');
+            $table->text('arquivo')->nullable();
+            $table->enum('status', ['ativo', 'inativo'])->default('ativo');
+            $table->foreignId('idPublicoAlvo')->constrained('publicos_alvos')->onDelete('cascade');
+            $table->foreignId('idCategoria')->constrained('categorias_solucoes')->onDelete('cascade');
+            $table->foreignId('idUsuario')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('solucoes');
+    }
+};

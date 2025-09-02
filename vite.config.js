545b895f6a6eb83197d7886c810236664077c899
Jamile Ramos <jamiledasilvaramos@gmail.com>
2025-09-01 22:12:27 -0300

    Correção no Vite

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true, // hot-reload no local
-            // não use buildDirectory
+            refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build', // saída final
-        manifest: true,         // gera manifest.json
-        emptyOutDir: true,      // limpa a pasta antes
-        rollupOptions: {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-        },
-    },
 });
